50f79bb990757513a4dc5925c8df1f98938b0f99
Higor <higorreis0@gmail.com>
2025-08-17 23:11:56 -0300

    Algumas_questoes_respondidas
    Até a questão 4 da parte de exercicioVar/Nome

--- exercicios_par_nome_valor.js
+++ exercicios_par_nome_valor.js
@@ -5,12 +5,32 @@ Exercícios Práticos - Par Nome/Valor
 */
 
 // 1. Declare uma variável string e exiba seu valor no console.
+var variavelString = "Beltrano"
+console.log(variavelString);
 
 // 2. Crie um objeto com pelo menos 3 pares nome/valor e exiba uma das propriedades.
+alunos = {
+    nome: "fulano",
+    idade: 16,
+    matricula: 10204
+}
 
+console.log(alunos.nome)
 // 3. Crie um array com 5 elementos e exiba o terceiro elemento.
 
+elementos = ["ar", "terra", "fogo", "agua"]
+
+console.log(elementos[2]);
+
 // 4. Crie um objeto e adicione uma nova propriedade dinamicamente.
+
+const funcionarios = {
+    nome: "Fulano",
+    idade: 30,
+}
+
+funcionarios.carteirinha = "1010"
+console.log(funcionarios);
 
 // 5. Mostre como acessar um valor de um objeto usando colchetes e uma variável.
 
